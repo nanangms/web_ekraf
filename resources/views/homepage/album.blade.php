0977ff031b4c3eaeeeddbdd4da17f90ccbaf15c7
Fikri Al Hakim <fikrialhakim54@gmail.com>
2021-03-31 02:11:32 +0700

    Update halaman foto & album pada homepage

---
+++ resources/views/homepage/album.blade.php
@@ -0,0 +1,21 @@
+@extends('homepage.layouts.app')
+
+@section('title')
+Album | EKRAF Jambi
+@endsection
+
+@section('content')
+
+<div class="container d-flex flex-column justify-content-center pt-7" style="flex: 1 0 auto;">
+    <div class="pt-7 pb-4">
+      <div class="text-center mb-2 pb-4">
+        <h1 class="mb-5">
+        	<span class="visually-hidden">Error 404</span>
+        </h1>
+        <h2>Halaman masih dalam tahap pengembangan</h2>
+        <p class="pb-2">Halaman ini belum tersedia untuk saat ini. Mohon cek lagi nanti.</p><a class="btn btn-translucent-primary me-3" href="/">Kembali ke halaman utama</a>
+      </div>
+    </div>
+  </div>
+
+@stop
