bd6eae002dbd48dc4e3f621537b2438eeca951fd
Fikri Al Hakim <fikrialhakim54@gmail.com>
2021-03-31 02:19:41 +0700

    Update halaman detail pelaku ekraf pada homepage

--- resources/views/homepage/pelaku_ekraf.blade.php
+++ resources/views/homepage/pelaku_ekraf.blade.php
@@ -5,37 +5,106 @@ Data Pelaku Ekraf | EKRAF Jambi
 @endsection
 
 @section('content')
-<div class="container mt-7 mb-2">
-  <div class="mb-5">
-    <div class="d-flex flex-wrap flex-md-nowrap justify-content-between">
-      <h3 class="widget-title text-nowrap">Pelaku Ekraf</h3>
-    </div>
-<div class="tns-carousel-wrapper pb-5 pb-lg-0">
-        		<div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 4, &quot;controls&quot;: false, &quot;gutter&quot;: 23, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;300&quot;:{&quot;items&quot;:2},&quot;600&quot;:{&quot;items&quot;:3},&quot;900&quot;:{&quot;items&quot;:4}}}">
-    @foreach ($pelaku_ekraf as $pelaku)
-				    <div>
-			        	<a class="nav-heading text-center" href="/data-pelaku-ekraf/{{$pelaku->kode_pelaku_ekraf}}">
-			        		<div class="card border-0 bg-transparent">
-			        			<div class="card-hover card-img card-img-gradient border-0 shadow mb-3">
-			        				<!-- <span class="badge badge-floating badge-floating-end bg-primary fs-sm py-2 px-3">Kuliner</span> -->
-			        				@if($pelaku->foto_usaha != Null)
-			    					<div class="member-thumbnail">
-									  <img src="{{ asset('images/foto_usaha/thumb/'.$pelaku->foto_usaha) }}"/>
-									</div>
-									@else
-									<div class="member-thumbnail">
-									  <img src="{{ asset('images/no_ekraf.jpg') }}"/>
-									</div>
-									@endif
-					            	<span class="card-floating-text text-light fw-medium">Lihat detail usaha<i class="fas fa-angle-right align-middle fs-lg ms-3"></i></span>
-					            </div>
-					            <h6 class="nav-heading-title mb-0">{{$pelaku->nama_usaha}}</h6>
-	                			<span class="fs-sm fw-normal text-muted">{{$pelaku->nama_sektor}}</span>
-			        		</div>
-				        </a>
-				    </div>
-				@endforeach
-			</div>
-  </div>
+<!-- Page content-->
+<div class="sidebar-enabled sidebar-end mt-7">
+	<div class="container">
+	  <div class="row">
+	    <div class="col-lg-9 content pt-4 pt-lg-5 mb-2 mb-sm-0 pb-sm-5">
+	      <h2 class="mb-5">Pelaku Ekraf</h2>
+	      <!-- Blog grid-->
+	      <div class="masonry-grid overflow-hidden" data-columns="4">
+	      	@foreach ($pelaku_ekraf as $pelaku)
+	      		<!-- Post-->
+		        <div class="masonry-grid-item">
+		          	<a class="nav-heading text-center" href="/data-pelaku-ekraf/{{$pelaku->kode_pelaku_ekraf}}">
+		        		<div class="card border-0 bg-transparent">
+		        			<div class="card-hover card-img card-img-gradient border-0 shadow mb-3">
+		        				<!-- <span class="badge badge-floating badge-floating-end bg-primary fs-sm py-2 px-3">Kuliner</span> -->
+		        				@if($pelaku->foto_usaha != Null)
+		    					<div class="member-thumbnail">
+								  <img src="{{ asset('images/foto_usaha/thumb/'.$pelaku->foto_usaha) }}"/>
+								</div>
+								@else
+								<div class="member-thumbnail">
+								  <img src="{{ asset('images/default_thumb_placeholder.jpg') }}"/>
+								</div>
+								@endif
+				            	<span class="card-floating-text text-light fw-medium">Lihat detail usaha<i class="fas fa-angle-right align-middle fs-lg ms-3"></i></span>
+				            </div>
+				            <h6 class="nav-heading-title mb-0">{{$pelaku->nama_usaha}}</h6>
+		        			<span class="fs-sm fw-normal text-muted">{{$pelaku->nama_sektor}}</span>
+		        		</div>
+		        	</a>
+		        </div>
+	      	@endforeach
+	      </div>
+
+	      <!-- Pagination-->
+		  <div class="pt-4 pb-2">
+		    <nav class="mb-4" aria-label="Page navigation">
+		      <ul class="pagination justify-content-center">
+		        <li class="page-item"><a class="page-link" href="#" aria-label="Previous"><i class="fas fa-angle-left"></i></a></li>
+		        <li class="page-item d-sm-none"><span class="page-link page-link-static">1 / 10</span></li>
+		        <li class="page-item active d-none d-sm-block" aria-current="page"><span class="page-link">1<span class="visually-hidden">(current)</span></span></li>
+		        
+		        <li class="page-item"><a class="page-link" href="#" aria-label="Next"><i class="fas fa-angle-right"></i></a></li>
+		      </ul>
+		    </nav>
+		  </div>
+	    </div>
+
+	    <!-- Sidebar-->
+	    <div class="sidebar col-lg-3 pt-lg-5">
+	      <div class="offcanvas offcanvas-end offcanvas-collapse" id="blog-sidebar">
+	        <div class="offcanvas-cap navbar-shadow px-4 mb-3">
+	          <h5 class="mt-1 mb-0">Sidebar</h5>
+	          <button class="btn-close lead" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+	        </div>
+	        <div class="offcanvas-body px-4 pt-3 pt-lg-0 pe-lg-0 ps-lg-2 ps-xl-4" data-simplebar>
+	        	<h3 class="widget-title">Cari pelaku ekraf</h3>
+	        	<form>
+		  			<div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="from-destination">Pelaku Ekraf</label>
+			          <div class="input-group">
+			          	<input type="text" class="form-control" name="" placeholder="Nama usaha/pemilik usaha">
+			          </div>
+			      </div>
+			      <div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="to-destination">Wilayah</label>
+			          <select class="form-select" id="to-destination">
+			            <option value="" selected disabled hidden>Kabupaten/Kota</option>
+			            <option value="Kota Jambi">Kota Jambi</option>
+			            <option value="Kota Sungai Penuh">Kota Sungai Penuh</option>
+			            <option value="Kabupaten Batanghari">Kabupaten Batanghari</option>
+			            <option value="Kabupaten Bungo">Kabupaten Bungo</option>
+			            <option value="Kabupaten Kerinci">Kabupaten Kerinci</option>
+			            <option value="Kabupaten Merangin">Kabupaten Merangin</option>
+			            <option value="Kabupaten Muaro Jambi">Kabupaten Muaro Jambi</option>
+			            <option value="Kabupaten Sarolangun">Kabupaten Sarolangun</option>
+			            <option value="Kabupaten Tanjung Jabung Timur">Kabupaten Tanjung Jabung Timur</option>
+			            <option value="Kabupaten Tanung Jabung Barat">Kabupaten Tanung Jabung Barat</option>
+			            <option value="Kabupaten Tebo">Kabupaten Tebo</option>
+			          </select>
+			        </div>
+			        <div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="to-destination">Sektor</label>
+			          <select class="form-select" id="to-destination">
+			            <option value="" selected disabled hidden>Sektor/bidang usaha</option>
+			            <option value="Sektor 1">Sektor 1</option>
+			            <option value="Sektor 2">Sektor 2</option>
+			            <option value="Sektor 3">Sektor 3</option>
+			          </select>
+			        </div>
+			        <div class="text-center text-sm-start mt-2 mt-sm-4">
+			          <button class="btn btn-primary w-100" type="submit">Cari pelaku Ekraf</button>
+			        </div>
+		  		</form>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 </div>
+
+
 @stop
--- resources/views/homepage/pelaku_ekraf_detail.blade.php
+++ resources/views/homepage/pelaku_ekraf_detail.blade.php
@@ -1,17 +1,228 @@
 @extends('homepage.layouts.app')
 
 @section('title')
-Detail Pelaku Ekraf | EKRAF Jambi
+Profil Pelaku Ekraf : {{$pelaku_ekraf->nama_usaha}} | EKRAF Jambi
 @endsection
 
 @section('content')
-<div class="container mt-7 mb-2">
-  <div class="mb-5">
-    <div class="d-flex flex-wrap flex-md-nowrap justify-content-between">
-      <h3 class="widget-title text-nowrap">Detail Pelaku Ekraf</h3>
-    </div>
+<div class="sidebar-enabled sidebar-end mt-7">
+	<div class="container">
+	  <div class="row">
+	    <div class="col-lg-9 content pt-4 pt-lg-3 mb-2 mb-sm-0 pb-sm-5">
+	      	<!-- Content-->
+          	<div class="d-sm-flex align-items-center justify-content-between pb-4 text-center text-sm-start">
+	         	<h1 class="h3 mb-2 text-nowrap">{{$pelaku_ekraf->nama_usaha}}</h1>
+	         	<!-- Nav tabs -->
+				<ul class="nav nav-tabs mt-4" role="tablist" style="font-size: 1rem">
+				  <li class="nav-item">
+				    <a href="#profil_usaha" class="nav-link active" data-bs-toggle="tab" role="tab">
+				      Profil usaha
+				    </a>
+				  </li>
+				  <li class="nav-item">
+				    <a href="#profil_pemilik" class="nav-link" data-bs-toggle="tab" role="tab">
+				      Profil pemilik usaha
+				    </a>
+				  </li>
+				  
+				</ul>
+	        </div>
+	        <div class="row mb-3">
+	        	<div class="col-lg-5">
+	        		<div class="member-image rounded-3 shadow-lg mb-4" style="overflow: hidden">
+				  	  @if($pelaku_ekraf->foto_usaha != Null)
+    					<img src="{{ asset('images/foto_usaha/'.$pelaku_ekraf->foto_usaha) }}"/>
+					  @else
+						<img src="{{ asset('images/default_placeholder.jpg') }}"/>
+					  @endif
+				  	</div>
+	        	</div>
+	        	<div class="col-lg-7">
+	        		<!-- Tabs content -->
+					<div class="tab-content">
+					  <div class="tab-pane fade show active" id="profil_usaha" role="tabpanel">
+					    <table class="table" width="100%" style="vertical-align: middle;">
+		                  	<tr>
+		                  		<td width="47%">Nama Usaha/Perusahaan</td>
+		                  		<td width="3%">:</td>
+		                  		<td>{{$pelaku_ekraf->nama_usaha}}</td>
+		                  	</tr>
+		                  	<tr>
+		                  		<td>Sektor Usaha</td>
+		                  		<td>:</td>
+		                  		<td>{{$pelaku_ekraf->nama_sektor}}</td>
+		                  	</tr>
+		                  	<tr>
+		                  		<td>Kota/Kabupaten</td>
+		                  		<td>:</td>
+		                  		<td>{{$pelaku_ekraf->nama_kab_kota}}</td>
+		                  	</tr>
+		                  	<tr>
+		                  		<td>Bentuk Usaha/Perusahaan</td>
+		                  		<td>:</td>
+		                  		<td>{{$pelaku_ekraf->nama_badan_hukum}}</td>
+		                  	</tr>
+		                  	<tr>
+		                  		<td>Tanggal Berdiri Usaha</td>
+		                  		<td>:</td>
+		                  		<td>{{TanggalAja($pelaku_ekraf->mulai_usaha)}}</td>
+		                  	</tr>
+		                </table>
+					  </div>
+					  <div class="tab-pane fade mt-4 mt-lg-0" id="profil_pemilik" role="tabpanel">
+					    <div class="d-block d-sm-flex align-items-center text-center text-sm-start border-bottom pb-3 mb-3">
+					    	@if($pelaku_ekraf->foto_pemilik != Null)
+		    					<div class="d-block rounded-3 mx-sm-0 mx-auto mb-3 mb-sm-0 bg-size-cover bg-position-center" style="background-image: url({{ asset('images/foto_pemilik/thumb/'.$pelaku_ekraf->foto_pemilik) }}); width: 100px; height: 100px">
+					    		</div>
+							@else
+								<div class="d-block shadow-3 rounded-3 mx-sm-0 mx-auto mb-3 mb-sm-0 bg-size-cover bg-position-center" style="background-image: url({{ asset('images/foto_pemilik/default.jpg') }}); width: 100px; height: 100px">
+					    		</div>
+							@endif
+		                    <div class="ps-0 ps-lg-3">
+		                      <h5 class="nav-heading mb-1">{{$pelaku_ekraf->nama_pemilik}}</h5>
+		                      <div class="fs-md text-muted mb-2 pb-1">{{$pelaku_ekraf->email_pemilik}}</div>
+		                      <span class="badge bg-primary p-2">{{$pelaku_ekraf->member}}</span>
+		                    </div>
+		                </div>
+		                <div>
+		                	<table class="table" width="100%" style="vertical-align: middle;">
+		                		<tr>
+		                			<td width="32%">No. Telpon / WA</td>
+		                			<td width="3%">:</td>
+		                			<td>{{$pelaku_ekraf->wa_pemilik}}</td>
+		                		</tr>
+		                		<tr>
+		                			<td>Media sosial</td>
+		                			<td>:</td>
+		                			<td>
+		                				<div>
+											<a href="#" class="btn-social bs-facebook">
+											  <i class="fab fa-facebook-f"></i>
+											</a>
 
-    Detail Pelaku Ekraf
-  </div>
+											<a href="#" class="btn-social bs-twitter">
+											  <i class="fab fa-twitter"></i>
+											</a>
+
+											<a href="#" class="btn-social bs-instagram">
+											  <i class="fab fa-instagram"></i>
+											</a>
+
+											<a href="#" class="btn-social bs-telegram">
+											  <i class="fab fa-telegram"></i>
+											</a>
+
+											<a href="#" class="btn-social bs-linkedin">
+											  <i class="fab fa-linkedin"></i>
+											</a>
+					                    </div>
+					                </td>
+		                		</tr>
+		                	</table>
+		                </div>
+					  </div>
+					</div>
+	        		
+	        	</div>
+	        </div>
+
+	        <div class="mb-4 pb-3 border-bottom">
+	        	<!-- Nav tabs -->
+				<ul class="nav nav-tabs" role="tablist">
+				  <li class="nav-item">
+				    <a href="#deskripsi" class="nav-link active" data-bs-toggle="tab" role="tab">
+				      Deskripsi
+				    </a>
+				  </li>
+				  <li class="nav-item">
+				    <a href="#keahlian" class="nav-link" data-bs-toggle="tab" role="tab">
+				      Keahlian
+				    </a>
+				  </li>
+				  <li class="nav-item">
+				    <a href="#pengalaman" class="nav-link" data-bs-toggle="tab" role="tab">
+				      Pengalaman
+				    </a>
+				  </li>
+				  <li class="nav-item">
+				    <a href="#portofolio" class="nav-link" data-bs-toggle="tab" role="tab">
+				      Portofolio
+				    </a>
+				  </li>
+				</ul>
+
+				<!-- Tabs content -->
+				<div class="tab-content">
+				  <div class="tab-pane fade show active" id="deskripsi" role="tabpanel">
+				    <p>{{$pelaku_ekraf->deskripsi}}</p>
+				  </div>
+				  <div class="tab-pane fade" id="keahlian" role="tabpanel">
+				    <p>{{$pelaku_ekraf->keahlian}}</p>
+				  </div>
+				  <div class="tab-pane fade" id="pengalaman" role="tabpanel">
+				    <p>{{$pelaku_ekraf->pengalaman}}</p>
+				  </div>
+				  <div class="tab-pane fade" id="portofolio" role="tabpanel">
+				    <p>{{$pelaku_ekraf->portofolio}}</p>
+				  </div>
+				</div>
+	        </div>
+
+	        <div>
+	        	<h5 class="text-nowrap mb-4 me-3">Kontak</h5>
+	        </div>
+	    </div>
+
+	    <!-- Sidebar-->
+	    <div class="sidebar col-lg-3 pt-lg-5">
+	      <div class="offcanvas offcanvas-end offcanvas-collapse" id="blog-sidebar">
+	        <div class="offcanvas-cap navbar-shadow px-4 mb-3">
+	          <h5 class="mt-1 mb-0">Sidebar</h5>
+	          <button class="btn-close lead" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+	        </div>
+	        <div class="offcanvas-body px-4 pt-3 pt-lg-0 pe-lg-0 ps-lg-2 ps-xl-4" data-simplebar>
+	        	<h3 class="widget-title">Cari pelaku ekraf</h3>
+	        	<form>
+		  			<div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="from-destination">Pelaku Ekraf</label>
+			          <div class="input-group">
+			          	<input type="text" class="form-control" name="" placeholder="Nama usaha/pemilik usaha">
+			          </div>
+			      </div>
+			      <div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="to-destination">Wilayah</label>
+			          <select class="form-select" id="to-destination">
+			            <option value="" selected disabled hidden>Kabupaten/Kota</option>
+			            <option value="Kota Jambi">Kota Jambi</option>
+			            <option value="Kota Sungai Penuh">Kota Sungai Penuh</option>
+			            <option value="Kabupaten Batanghari">Kabupaten Batanghari</option>
+			            <option value="Kabupaten Bungo">Kabupaten Bungo</option>
+			            <option value="Kabupaten Kerinci">Kabupaten Kerinci</option>
+			            <option value="Kabupaten Merangin">Kabupaten Merangin</option>
+			            <option value="Kabupaten Muaro Jambi">Kabupaten Muaro Jambi</option>
+			            <option value="Kabupaten Sarolangun">Kabupaten Sarolangun</option>
+			            <option value="Kabupaten Tanjung Jabung Timur">Kabupaten Tanjung Jabung Timur</option>
+			            <option value="Kabupaten Tanung Jabung Barat">Kabupaten Tanung Jabung Barat</option>
+			            <option value="Kabupaten Tebo">Kabupaten Tebo</option>
+			          </select>
+			        </div>
+			        <div class="mb-3 pb-1 w-100 mb-sm-4 me-sm-3">
+			          <label class="form-label" for="to-destination">Sektor</label>
+			          <select class="form-select" id="to-destination">
+			            <option value="" selected disabled hidden>Sektor/bidang usaha</option>
+			            <option value="Sektor 1">Sektor 1</option>
+			            <option value="Sektor 2">Sektor 2</option>
+			            <option value="Sektor 3">Sektor 3</option>
+			          </select>
+			        </div>
+			        <div class="text-center text-sm-start mt-2 mt-sm-4">
+			          <button class="btn btn-primary w-100" type="submit">Cari pelaku Ekraf</button>
+			        </div>
+		  		</form>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 </div>
 @stop
